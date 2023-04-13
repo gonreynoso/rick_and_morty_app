69c20114cd800a1802c8ba148814bf4cecc4b825
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2023-04-13 17:47:14 -0300

    agregue archivos de redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 
 require('bootstrap/dist/css/bootstrap.css');
 
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
